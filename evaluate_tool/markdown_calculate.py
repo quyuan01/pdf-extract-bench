4fc2c1530cc9e4c28cab1e25fd7ed6bc10b1dc3a
quyuan <quyuan@pjlab.org>
2024-06-14 17:54:00 +0800

    fix index calculate

--- evaluate_tool/markdown_calculate.py
+++ evaluate_tool/markdown_calculate.py
@@ -1,82 +1,122 @@
 import os  
 from Levenshtein import distance  
-from nltk.translate.bleu_score import corpus_bleu  
+from nltk.translate.bleu_score import sentence_bleu, SmoothingFunction, corpus_bleu
 from nltk.tokenize import word_tokenize  
 import json 
+import re
+import scoring
+import argparse
+
 parser = argparse.ArgumentParser(description="get directory")
 parser.add_argument(
-    "--standard ",
+    "--download_dir",
     type=str,
     required=True,
-    help="input standard path",
-)
-
-parser.add_argument(
-    "--actual",
-    type=str,
-    required=True,
-    help="input actual path",
+    help="input download dir",
 )
 parser.add_argument(
     "--results ",
     type=str,
     required=True,
-    help="results path",
+    help="results path(end with .json)",
 )
 args = parser.parse_args()
 
-class EvaluationTool:
-    def __init__(self, standard_dir, actual_dir, results_dir):
-        self.standard_dir = standard_dir
-        self.actual_dir = actual_dir
-        self.results = results_dir
+# 初始化列表来存储编辑距离和BLEU分数  
+class Scoring:
+    def __init__(self):
         self.edit_distances = []
         self.bleu_scores = []
+        self.sim_scores = []
         self.filenames = []
-        self.output = dict()
-    def get_scores(self):
-        for filename in os.listdir(self.actual_dir):  
-            if filename.endswith('.md') and not filename.startswith('.'): 
-                with open(os.path.join(self.dir_a, filename), 'r', encoding='utf-8') as file_a:  
-                    content_a = file_a.read() 
-  
-                filepath_b = os.path.join(self.standard_dir, filename)  
+        self.score_dict = {}
+        self.anntion_cnt = 0
+        self.fw = open(args.results, 'w+', encoding='utf-8')
+    def simple_bleu_score(self, candidate, reference):  
+        candidate_tokens = word_tokenize(candidate)  
+        reference_tokens = word_tokenize(reference) 
+        return sentence_bleu([reference_tokens], candidate_tokens, smoothing_function=SmoothingFunction().method1) 
+
+
+    def preprocess_string(self, s):  
+        sub_enter = re.sub(r'\n+', '\n', s)
+        return re.sub(r'  ', ' ', sub_enter)
+    
+    def calculate_similarity(self, annotion, actual, tool_type):
+        class_dict = {}
+        edit_distances = []
+        bleu_scores = []
+        sim_scores = list()
+        total_file = 0
+        for filename in os.listdir(annotion):  
+            if filename.endswith('.md') and not filename.startswith('.'):  # 忽略隐藏文件  
+                total_file = total_file + 1
+                # 读取A目录中的文件  
+                with open(os.path.join(annotion, filename), 'r', encoding='utf-8') as file_a:  
+                    content_a = file_a.read()
+                self.anntion_cnt = self.anntion_cnt + 1
+                filepath_b = os.path.join(actual, filename)  
                 if os.path.exists(filepath_b):  
                     with open(filepath_b, 'r', encoding='utf-8') as file_b:  
-                        content_b = file_b.read()  
+                        content_b = file_b.read()
                         self.filenames.append(filename)
-                        # 计算编辑距离  
-                        edit_dist = distance(content_a, content_b) / max(len(content_a), len(content_b))
+                        # 计算编辑距离
+                        edit_dist = distance(self.preprocess_string(content_b),self.preprocess_string(content_a)) / max(len(content_a), len(content_b))
                         self.edit_distances.append(edit_dist)  
+                        edit_distances.append(edit_dist)
                         #计算BLUE分数
-                        bleu_score = bleu_score(content_a, content_b)  
+                        bleu_score = self.simple_bleu_score(content_b, content_a)  
+                        bleu_scores.append(bleu_score)
                         self.bleu_scores.append(bleu_score)  
-                        self.output[filename] = {
-                            'edit_distance': edit_dist,
-                            'bleu_score': bleu_score
-                        }
+                        #计算marker分数
+                        score = scoring.score_text(content_b, content_a)
+                        sim_scores.append(score)
+                        self.sim_scores.append(score)
+                        class_dict[filename] = {"edit_dist": edit_dist, "bleu_score": bleu_score, "score": score}
+                        self.score_dict[filename] = {"edit_dist": edit_dist, "bleu_score": bleu_score, "score": score}
                 else:  
-                    print(f"File {filename} not found in directory standard_dir.")  
-        average_edit_distance = sum(self.edit_distances) / len(self.edit_distances) 
-        average_bleu_score = sum(self.bleu_scores) / len(self.bleu_scores) 
-        
-        # 输出结果到文件
-        with open(os.path.join(self.results, 'results.txt'), 'w', encoding='utf-8') as fw:  
-            fw.write(f"Average Levenshtein Distance: {average_edit_distance}\n")
-            fw.write(f"Average BLEU Score: {average_bleu_score}" + "\n")
-            fw.write(json.dumps(self.output, ensure_ascii=False) + "\n")
-            fw.close()
-def bleu_score(candidate, reference):  
-    candidate_tokens = word_tokenize(candidate)  
-    reference_tokens = word_tokenize(reference) 
-    return corpus_bleu([[reference_tokens]], [candidate_tokens])  
+                    print(f"File {filename} not found in actual directory.")  
+        # 计算每类平均值
+        class_average_edit_distance = sum(edit_distances) / len(edit_distances) if edit_distances else 0  
+        class_average_bleu_score = sum(bleu_scores) / len(bleu_scores) if bleu_scores else 0  
+        class_average_sim_score = sum(sim_scores) / len(sim_scores) if sim_scores else 0
+        self.fw.write(json.dumps(class_dict, ensure_ascii=False) + "\n")
+        ratio = len(class_dict)/total_file
+        self.fw.write(f"{tool_type} extract ratio:  {ratio}" + "\n")
+        self.fw.write(f"{tool_type} Average Levenshtein Distance: {class_average_edit_distance}" + "\n")
+        self.fw.write(f"{tool_type} Average BLEU Score: {class_average_bleu_score}" + "\n")
+        self.fw.write(f"{tool_type} Average Marker Score: {class_average_sim_score}" + "\n")
 
-if __name__ == "__main__":
-    standard_dir = args.standard
-    actual_dir = args.actual
-    results_dir = args.results
-    evaluation_tool = EvaluationTool(standard_dir, actual_dir, results_dir)
-    evaluation_tool.get_scores()
+        print (f"{tool_type} extract ratio: {ratio}")
+        print (f"{tool_type} Average Levenshtein Distance: {class_average_edit_distance}")
+        print (f"{tool_type} Average BLEU Score: {class_average_bleu_score}")
+        print (f"{tool_type} Average Marker Score: {class_average_sim_score}")
 
+        # 计算整体平均值
+        average_edit_distance = sum(self.edit_distances) / len(self.edit_distances) if self.edit_distances else 0  
+        average_bleu_score = sum(self.bleu_scores) / len(self.bleu_scores) if self.bleu_scores else 0  
+        average_sim_score = sum(self.sim_scores) / len(self.sim_scores) if self.sim_scores else 0
+        #self.fw.write(json.dumps(self.score_dict, ensure_ascii=False) + "\n")
+        self.fw.write(f"all extract cnt: {len(self.score_dict)/self.anntion_cnt}" + "\n")
+        self.fw.write(f"all Average Levenshtein Distance: {average_edit_distance}" + "\n")
+        self.fw.write(f"all Average BLEU Score: {average_bleu_score}" + "\n")
+        self.fw.write(f"all Average Marker Score: {average_sim_score}" + "\n") 
+        print ("extract ratio: ", len(self.score_dict)/self.anntion_cnt)
+        print (f"Average Levenshtein Distance: {average_edit_distance}")
+        print (f"Average BLEU Score: {average_bleu_score}")
+        print (f"Average Marker Score: {average_sim_score}")
+        return self.score_dict
 
+ 
+    def calculate_similarity_total(self, tool_type, file_types, download_dir):
+        for file_type in file_types:
+            annotion = r"%s\%s\annotations\cleaned" % (download_dir, file_type)
+            actual = r"%s\%s\%s\cleaned" % (download_dir, file_type, tool_type)
+            self.calculate_similarity(annotion, actual, file_type)
 
+if __name__ == "__main__":  
+  file_types = ["academic_literature", "atlas", "courseware", "colorful_textbok", "historical_documents", "notes", "ordinary_books", "ordinary_exam_paper", "ordinary_textbook", "research_report", "special_exam_paper"]
+  tool_type =args.tool_type
+  download_dir = args.download_dir
+  score = Scoring()
+  score.calculate_similarity_total(tool_type, file_types, download_dir)
