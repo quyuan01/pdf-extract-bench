84d3b92a55a90f7bdc9e8bcffbb4a2f9ec9ffae7
quyuan <quyuan@pjlab.org>
2024-06-21 19:24:38 +0800

    update zh readme

--- datasets/download_dataset.py
+++ datasets/download_dataset.py
@@ -1,19 +1,14 @@
+import openxlab
+openxlab.login(ak=<Access Key>, sk=<Secret Key>) #进行登录，输入对应的AK/SK
 
-  pip install openxlab #安装
+from openxlab.dataset import info
+info(dataset_repo='OpenDataLab/Miner-PDF-Benchmark') #数据集信息查看
 
-  pip install -U openxlab #版本升级
-  
-  import openxlab
-  openxlab.login(ak=<Access Key>, sk=<Secret Key>) #进行登录，输入对应的AK/SK
+from openxlab.dataset import query
+query(dataset_repo='OpenDataLab/Miner-PDF-Benchmark') #数据集文件列表查看
 
-  from openxlab.dataset import info
-  info(dataset_repo='quyuan/Miner-PDF-Benchmark') #数据集信息查看
+from openxlab.dataset import get
+get(dataset_repo='OpenDataLab/Miner-PDF-Benchmark', target_path='.')  # 数据集下载
 
-  from openxlab.dataset import query
-  query(dataset_repo='quyuan/Miner-PDF-Benchmark') #数据集文件列表查看
-
-  from openxlab.dataset import get
-  get(dataset_repo='quyuan/Miner-PDF-Benchmark', target_path='/path/to/local/folder/')  # 数据集下载
-
-  from openxlab.dataset import download
-  download(dataset_repo='quyuan/Miner-PDF-Benchmark',source_path='/README.md', target_path='/path/to/local/folder') #数据集文件下载
+from openxlab.dataset import download
+download(dataset_repo='OpenDataLab/Miner-PDF-Benchmark',source_path='/README.md', target_path='.') #数据集文件下载
